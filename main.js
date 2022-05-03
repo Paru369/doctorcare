042eef015dab66e3e81c8f367c821e26cef754bb
Paru - Paulo Victor Pinheiro <paruvitu@gmail.com>
2022-05-03 16:09:33 -0300

    fim dia 2

--- main.js
+++ main.js
@@ -2,10 +2,19 @@
 
 function onScroll(){
 
-  if(scrollY > 50){
-  navigation.classList.add('scroll')
-  } else {
-    navigation.classList.remove('scroll')
+  (scrollY > 30) ? navigation.classList.add('scroll') : navigation.classList.remove('scroll')
 
-  }
+
+}
+
+function openMenu() {
+
+  document.body.classList.add('menu-expended')
+
+}
+
+function closeMenu() {
+
+  document.body.classList.remove('menu-expended')
+
 }
