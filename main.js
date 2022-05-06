895d41a7c7830e3c982cb11c6afd9bfaaf686ebe
Paru - Paulo Victor Pinheiro <99094718+Paru369@users.noreply.github.com>
2022-05-06 17:55:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,57 @@ function onScroll(){
 
   showNavOnScroll()
   showBackToTopButton()
+
+  
+  activateMenu(home)
+  activateMenu(services)
+  activateMenu(about)
+  activateMenu(contact)
+
+
+}
+
+
+function activateMenu(section){
+
+  const targetLine = scrollY + innerHeight /2
+
+  // Verificando se o top passou da linha 
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  // Verificando se a base esta abaixo da linha 
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  //limites da sessao
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  //pegando o ID da sessão
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+
+  // colocando a classe ativa
+  
+    menuElement.classList.remove('active')
+    if (sectionBoundaries){
+    console.log(menuElement)
+
+    menuElement.classList.add('active')
+
+
+}
+
+
+
+
+}
+
+
   
 function showNavOnScroll(){
   (scrollY > 30) ? navigation.classList.add('scroll') : navigation.classList.remove('scroll')
@@ -15,8 +66,6 @@ function showBackToTopButton(){
   
   (scrollY > 400) ?  backToTopButton.classList.add('show') : backToTopButton.classList.remove('show')
 
-
-}
 
 }
 
